473e659132c2173cbdf97e0a583ef0f25bf444a0
SriyansuDash <sriyansudash09@gmail.com>
2024-12-02 20:38:56 +0530

    Next file Added

---
+++ io.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter a number ";
+    cin >> n ;
+    cout << "The value of n is : " << n << "\n";
+    // string s ;
+    // cout << "Enter a string \n";
+    // cin >> s ;
+    // cout <<"The string is : "<< s << endl;
+    double a = 0.45;
+    cout<< "The value of double a is : "<< a << endl;
+    cout << "Enter two strings \n";
+    string s1;
+    string s2;
+    cin >> s1 >> s2;
+    std::cout << s1 << " "<<s2 << endl;
+    // cout<< "Enter the string line " << endl;
+    // string line;
+    // getline(cin, line);
+    // cout << line << endl;
+    char ch ;
+    cout << "Enter a character \n";
+    cin >> ch;
+    cout << "The character is : "<< ch << endl;
+    return 0;
+}
