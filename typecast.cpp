bf605f683b54e3aa07cb3824351f5fb36b533943
SriyansuDash <sriyansudash09@gmail.com>
2025-04-02 11:54:14 +0530

    typecast.cpp

---
+++ typecast.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a = 'a';
+    cout << "The ascii value of a is : " << a << endl;
+    
+    char b = 92;
+    cout << "The character value of b is : " << b << endl;
+    return 0;
+}
+
