bdc3d4ab7ba3a989fb353dceee35781317883fa1
SriyansuDash <sriyansudash09@gmail.com>
2025-04-04 02:00:45 +0530

    loops/while_loop.cpp

---
+++ loops/while_loop.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "Enter the sequence of number " << endl;
+    int number;
+    cin >> number ;
+    cout << "\n";
+    int i = 0;
+    while (i < number)
+    {
+        cout << i << "\n";
+        i++;
+    }
+    return 0;
+}
