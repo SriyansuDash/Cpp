d882899595693cdbbb34317bed0669b17d4b7c7e
SriyansuDash <sriyansudash09@gmail.com>
2025-04-09 01:45:34 +0530

    Arrays.cpp

---
+++ Arrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int numbers[7] = {0,1,2,3,4,5,6};
+    // cout << numbers[0] << " " << numbers[1] << endl;
+    for(int i : numbers){
+        cout << i << " " << endl;
+    }
+    return 0;
+}
