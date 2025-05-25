55197b495fb2b717b42c702bf533a8047a9a5ca0
SriyansuDash <sriyansudash09@gmail.com>
2025-05-26 01:00:53 +0530

    array_size

--- Array_size.cpp
+++ Array_size.cpp
@@ -2,12 +2,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int myNumbers[5] = {10 ,20 , 30 , 40 ,50};
-    int getArrayLength = sizeof(myNumbers[0]);
-    for (int i = 0; i < 5; i++) {
-        cout << myNumbers[i] << "\n";
-      }
-    // cout << getArrayLength << endl;
-    return 0; 
+int main()
+{
+  int myNumbers[5] = {10, 20, 30, 40, 50};
+  int getArraySize = sizeof(myNumbers) / sizeof(myNumbers[0]);
+  cout << "Size of array is : " << getArraySize << endl;
+  cout << "Array elements are : ";
+  for (int i : myNumbers)
+  {
+    cout << i << " ";
+  }
+  return 0;
 }
