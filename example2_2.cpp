6c340635154606abb0f64ca3954de80016a5db0d
SriyansuDash <sriyansudash09@gmail.com>
2025-07-01 22:40:17 +0530

    example2_2.cpp

---
+++ example2_2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << " My name is Sriyansu Dash" << endl;
+    cout <<" My father's name is Uma Sankar Dash" << endl;
+    cout << " My mother's name is Pratima Dash" << endl;
+    return 0 ;
+}
